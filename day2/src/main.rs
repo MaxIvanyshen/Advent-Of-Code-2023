55cc0a9804147148c59cf23e99c5b78248f2c1f1
MaxIvanyshen <maxivanyshen@gmail.com>
2023-12-02 18:57:38 +0200

    add complete kinda clean day 2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,51 +1,68 @@
 use std::fs::read_to_string;
 
-fn get_id_if_valid(game_str: &str) -> i32 {
-    let conf = (12, 13, 14);     
+
+fn solve(game_str: &str) -> (i32, i32) {
+    let mut id: i32;
+    let mut power: i32;
+
     let game_str_split = game_str.split(":").collect::<Vec<_>>();
 
     let id_str = game_str_split[0].split(" ").collect::<Vec<_>>()[1].to_string();
-    let id = id_str.parse::<i32>().unwrap();
-    
-    let iterations = game_str_split[1].split(";").collect::<Vec<_>>();
 
+    id = id_str.parse::<i32>().unwrap();
+    
+    let mut iterations = game_str_split[1].split(";").collect::<Vec<_>>();
     for it in iterations {
 
         let colors = it.split(",");
-
+        
         for color in colors {
 
             let n_str = color.split(" ").collect::<Vec<_>>()[1];
             let n = n_str.parse::<i32>().unwrap();
 
             if color.contains("red") {
-                if n > conf.0 {
-                    return 0;
+                if n > 12 {
+                    id = 0;
+                }
+                if n > set.0 {
+                    set.0 = n;
                 }
             }
             else if color.contains("green") {
-                if n > conf.1 {
-                    return 0;
+                if n > 13 {
+                    id = 0;
+                }
+                if n > set.1 {
+                    set.1 = n;
                 }
             }
             else if color.contains("blue") {
-                if n > conf.2 {
-                    return 0;
+                if n > 14 {
+                    id = 0;
+                }
+                if n > set.2 {
+                    set.2 = n;
                 }
             }
         }
     }
 
+    power = set.0 * set.1 * set.2;
 
-    return id;
+    return (id, power);
 }
 
 fn main() {
-    let mut ans = 0;
+
+    let mut ans = (0, 0);
 
     for line in read_to_string("input").unwrap().lines() {
-        ans += get_id_if_valid(line);
+        let res = solve(line);
+        ans.0 += res.0;
+        ans.1 += res.1;
     }
 
-    println!("{}", ans);
+    println!("Part 1: {}", ans.0);
+    println!("Part 2: {}", ans.1);
 }
