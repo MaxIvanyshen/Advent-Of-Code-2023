8a9315dcb23d5e48b1652b1560ce8c89a7830992
MaxIvanyshen <maxivanyshen@gmail.com>
2023-12-02 18:15:21 +0200

    day 2 part 1 solution

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/home/max/Documents/Coding/adventOfCode/day2/target/debug/day2: /home/max/Documents/Coding/adventOfCode/day2/src/main.rs
---
+++ day2/target/debug/deps/day2-0b312c62a219b418.d
@@ -0,0 +1,5 @@
+/home/max/Documents/Coding/adventOfCode/day2/target/debug/deps/day2-0b312c62a219b418: src/main.rs
+
+/home/max/Documents/Coding/adventOfCode/day2/target/debug/deps/day2-0b312c62a219b418.d: src/main.rs
+
+src/main.rs:
