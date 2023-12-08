abfb2b7929b2f0b7d26ff76b15f768fe99f85f20
MaxIvanyshen <maxivanyshen@gmail.com>
2023-12-08 16:38:30 +0200

    solution with stack overflow

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 use std::collections::HashMap;
 
-fn dfs(curr_node: &str, map: &HashMap<&str, (&str, &str)>, steps: i32, path: Vec<char>, curr_step: usize) -> i32 {
+fn dfs_p1(curr_node: &str, map: &HashMap<&str, (&str, &str)>, steps: i32, path: Vec<char>, curr_step: usize) -> i32 {
     if curr_node.eq("ZZZ") {
         return steps ;
     }
@@ -14,11 +14,49 @@ fn dfs(curr_node: &str, map: &HashMap<&str, (&str, &str)>, steps: i32, path: Vec
     }
 
     if next == 'L' {
-        return dfs(map.get(curr_node).unwrap().0, map, steps + 1, path, next_step);
+        return dfs_p1(map.get(curr_node).unwrap().0, map, steps + 1, path, next_step);
     } 
     else {
-        return dfs(map.get(curr_node).unwrap().1, map, steps + 1, path, next_step);
+        return dfs_p1(map.get(curr_node).unwrap().1, map, steps + 1, path, next_step);
     } 
+}
+
+fn dfs_p2<'a>(curr_nodes: &'a mut Vec<&'a str>, map: &HashMap<&'a str, (&'a str, &'a str)>, steps: i32, path: Vec<char>, curr_step: usize) -> i32 {
+    let mut all_z = true;
+    for node in &mut *curr_nodes {
+        if !node.ends_with("Z") {
+            all_z = false; 
+        }
+    }
+    if all_z {
+        return steps;
+    }
+
+    let mut next_step = curr_step + 1;
+
+    let next = path[curr_step];
+    if curr_step + 1 >= path.len() {
+        next_step = 0;
+    }
+
+    let mut v: Vec<&str> = Vec::new();
+
+    if next == 'L' {
+        let mut i: usize = 0;
+        while i < curr_nodes.len() {
+            curr_nodes[i] = map.get(curr_nodes[i]).unwrap().0;
+            i += 1;
+        }
+        return dfs_p2(curr_nodes, map, steps + 1, path, next_step);
+    }
+    else {
+        let mut i: usize = 0;
+        while i < curr_nodes.len() {
+            curr_nodes[i] = map.get(curr_nodes[i]).unwrap().1;
+            i += 1;
+        }
+        return dfs_p2(curr_nodes, map, steps + 1, path, next_step);
+    }
 }
 
 fn main() {
@@ -48,5 +86,16 @@ fn main() {
         map.insert(value, (&v[0][1..v[0].len()], &v[1][1..v[1].len() - 1]));
      }
 
-     println!("{}", dfs("AAA", &map, 0, path, 0));
+     //println!("Part 1: {}", dfs_p1("AAA", &map, 0, path, 0));
+     
+     let mut nodes: Vec<&str> = Vec::new();
+     for (k, v) in map.iter() {
+        if k.ends_with("A") {
+            nodes.push(k);
+        }
+     }
+
+     println!("Part 2: {}", dfs_p2(&mut nodes, &map, 0, path, 0));
+
+
 }
